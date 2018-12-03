a745d31db26346fdd8b27873cf2acea9d21a89c5
Lucas <lucas.bicalho.oliveira@gmail.com>
2018-12-02 23:49:12 -0200

    Update Application.php

--- src/Application.php
+++ src/Application.php
@@ -38,6 +38,8 @@ class Application extends BaseApplication
         // Call parent to load bootstrap from files.
         parent::bootstrap();
 
+        $this->addPlugin('ADmad/HybridAuth');
+        
         if (PHP_SAPI === 'cli') {
             try {
                 $this->addPlugin('Bake');
